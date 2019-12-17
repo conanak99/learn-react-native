dd729d69cc143b9d2ce251b98ea44d159e26b174
conanak99 <huyhoang8a5@gmail.com>
2019-12-17 18:52:57 +0800

    refactor getimage

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -2,10 +2,18 @@ import axios from 'axios'
 
 import env from '../config'
 
-export const getImages = async (name: string): Promise<string[]> => {
-    const url = `https://bingsearch-codedao.cognitiveservices.azure.com/bing/v7.0/images/search?q="${name}"&count=10&safeSearch=Moderate&mkt=en-US&imageType=Photo&aspect=tall`
+export const getImages = async (name: string, count = 4): Promise<string[]> => {
+    const url = `https://bingsearch-codedao.cognitiveservices.azure.com/bing/v7.0/images`
     const response = await axios(url, {
         headers: {
+            params: {
+                name: `"${name}"`,
+                count,
+                safeSearch: 'Moderate',
+                mkt: 'en-US',
+                imageType: 'Photo',
+                aspect: 'tall',
+            },
             'Ocp-Apim-Subscription-Key': env.BING_API_KEY,
         },
     })
